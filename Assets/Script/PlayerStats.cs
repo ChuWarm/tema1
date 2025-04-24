6bf8747b71892109cb45441367ce48c9a2b7460c
SeJin <ion7678@gmail.com>
2025-04-24 18:54:26 +0900

    기본 스텟
    렙업시증가하는 수치등구현

---
+++ Assets/Script/PlayerStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int PlayerLevel = 1;
+    public int currentExp = 0;
+    public int expToNextLevel = 100;
+    
+    public int maxHealth = 100;
+    public int stamina = 10;
+    public int currentHealth = 100;
+    public int currentstamina = 10;
+    public int attackPower = 10;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    public void GainExperience(int amount)
+    {
+        currentExp += amount;
+        while (currentExp >= expToNextLevel)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        PlayerLevel++;
+        currentExp -= expToNextLevel;
+        expToNextLevel += 10;// 레벨업 요구량 증가
+        
+        maxHealth += 10;
+        attackPower += 10;
+        stamina += 2;
+        
+        currentHealth = maxHealth;
+        currentstamina = stamina;
+    }
+
+}
