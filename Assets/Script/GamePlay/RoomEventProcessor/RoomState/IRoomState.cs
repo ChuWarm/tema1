7ebbb23df52a27478b51287b3fb4aea6f4f94390
hyeons820 <hyeons82073@gmail.com>
2025-04-29 13:35:07 +0900

    RoomEventProcessor 생성

---
+++ Assets/Script/GamePlay/RoomEventProcessor/RoomState/IRoomState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRoomState
+{
+    void Enter(RoomEventProcessor processor);
+    void Update(RoomEventProcessor processor);
+}
