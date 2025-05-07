321ed0e87021791829bf6949ad00ef9c6ce370ab
SeJin <ion7678@gmail.com>
2025-05-07 14:38:23 +0900

    cs파일 정리, 캐릭터cs패치

---
+++ Assets/Script/Characters/PlayerStateAnim/PlayerStateAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerStateAttack : IPlayerState
+{
+    private PlayerController _player;
+    
+    public void EnterState(PlayerController playerController)
+    {
+        _player = playerController;
+    }
+
+    public void UpdateState()
+    {
+        
+    }
+
+    public void ExitState()
+    {
+        _player = null;
+    }
+}
---
+++ Assets/Script/Data/MemoryUpgradeData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MemoryUpgradeData : IGameData
+{
+    public enum UpgradeTpye
+    {
+        Stat
+    }
+
+    public string upgradeID;
+    public string upgradeName;
+    public UpgradeTpye upgradeType;
+    public string upgradeIconID;
+    public string targetStatOrSkill;
+    public float upgradeValue;
+    public int requiredMemoryPoint;
+    public string prerequisiteUpgradeID;
+    public string upgradeDescription;
+}
+
+
+[System.Serializable]
+public class MemoryUpgradeDataSheet
+{
+    public MemoryUpgradeData[] memoryUpgradeSheet;
+}
